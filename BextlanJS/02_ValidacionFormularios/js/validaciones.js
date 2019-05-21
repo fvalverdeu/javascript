fabfe7f3dcdfcca44dbfba97b7c5377e86238bde
Fernando Valverde Uchoffen <fvalverdeu@gmail.com>
2019-05-20 22:23:13 -0500

    Configuración inicial del repositorio de prácticas de JavaScript.

---
+++ BextlanJS/02_ValidacionFormularios/js/validaciones.js
@@ -0,0 +1,21 @@
+function validarForm(){
+    var verificar = true;
+    alert("Validando");
+    document.contacto-frm.submit();
+}
+
+function limpiarForm(){
+    alert("Limpiando");
+    document.getElementById("contacto-frm").reset();
+}
+
+window.onload = function(){
+    var botonEnviar, botonLimpiar;
+    botonLimpiar = document.getElementById("limpiar");
+    botonLimpiar.onclick = limpiarForm;
+
+    botonEnviar = document.contacto-frm.enviar_btn;
+    botonEnviar.onclick = validarForm;
+}
+
+
