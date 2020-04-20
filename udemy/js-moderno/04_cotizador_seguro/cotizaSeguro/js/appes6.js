d056b667c6d6165f815e0574a35598b328acc332
Fernando Valverde <fvalverdeu@gmail.com>
2020-04-19 21:35:43 -0500

    update

--- udemy/js-moderno/04_cotizador_seguro/cotizaSeguro/js/appes6.js
+++ udemy/js-moderno/04_cotizador_seguro/cotizaSeguro/js/appes6.js
@@ -1,75 +1,73 @@
 // cotizador constructor
 
-function Seguro(marca, anio, tipo) {
-  this.marca = marca;
-  this.anio = anio;
-  this.tipo = tipo;
-}
-
-// Seguro.prototype.cotizarSeguro = function(informacion) {
-//   console.log(informacion);
-// }
-
-Seguro.prototype.cotizarSeguro = function() {
-  /*
-    1 = americano   1.15
-    2 = asiático    1.05
-    3 = europeo     1.35
-  */
-  let cantidad;
-  const base = 2000;
-
-  switch (this.marca) {
-    case '1':
-      cantidad = base * 1.15;
-      break;
-    case '2':
-      cantidad = base * 1.05;
-      break;
-    case '3':
-      cantidad = base * 1.35;
-      break;
+class Seguro {
+  constructor (marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
   }
 
-  // Leer el año
-  const diferencia = new Date().getFullYear() - this.anio;
-  // Cada año de diferencia hay que reducir 3% el valor del seguro
-  cantidad -= ((diferencia * 3) * cantidad) / 100;
-
-  /*
-    Si el seguro es básico, se multiplica por 30% más.
-    Si el seguro es completo 50% más.
-  */
-
-  if(this.tipo === 'basico') {
-    cantidad *= 1.30;
-  } else {
-    cantidad *= 1.50;
+  cotizarSeguro() {
+    /*
+      1 = americano   1.15
+      2 = asiático    1.05
+      3 = europeo     1.35
+    */
+    let cantidad;
+    const base = 2000;
+  
+    switch (this.marca) {
+      case '1':
+        cantidad = base * 1.15;
+        break;
+      case '2':
+        cantidad = base * 1.05;
+        break;
+      case '3':
+        cantidad = base * 1.35;
+        break;
+    }
+  
+    // Leer el año
+    const diferencia = new Date().getFullYear() - this.anio;
+    // Cada año de diferencia hay que reducir 3% el valor del seguro
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+  
+    /*
+      Si el seguro es básico, se multiplica por 30% más.
+      Si el seguro es completo 50% más.
+    */
+  
+    if(this.tipo === 'basico') {
+      cantidad *= 1.30;
+    } else {
+      cantidad *= 1.50;
+    }
+    return cantidad;
   }
-  return cantidad;
 }
+
 
 // Todo lo que se muestra
-function Interfaz() {}
-
-// Mensaje que se imprime en el HTML
-Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
-  const div = document.createElement('div');
-  if (tipo === 'error') {
-    div.classList.add('mensaje','error');
-  } else {
-    div.classList.add('mensaje','correcto');
+class Interfaz {
+  // Mensaje que se imprime en el HTML
+  mostrarMensaje(mensaje, tipo) {
+    const div = document.createElement('div');
+    if (tipo === 'error') {
+      div.classList.add('mensaje','error');
+    } else {
+      div.classList.add('mensaje','correcto');
+    }
+    div.innerHTML = `${mensaje}`;
+    formulario.insertBefore(div, document.querySelector('.form-group'));
+  
+    setTimeout(function() {
+      document.querySelector('.mensaje').remove();
+    }, 3000);
   }
-  div.innerHTML = `${mensaje}`;
-  formulario.insertBefore(div, document.querySelector('.form-group'));
 
-  setTimeout(function() {
-    document.querySelector('.mensaje').remove();
-  }, 3000);
-}
-
-// Imprime el resultado de la cotización
-Interfaz.prototype.mostrarResultado = function(seguro, total) {
+  // Imprime el resultado de la cotización
+mostrarResultado(seguro, total) {
   const resultado = document.getElementById('resultado');
   let marca;
 
@@ -104,6 +102,9 @@ Interfaz.prototype.mostrarResultado = function(seguro, total) {
     resultado.appendChild(div);
   }, 3000);
 }
+}
+
+
 
 // EventListener
 const formulario = document.getElementById('cotizar-seguro');
